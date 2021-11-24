bba6da84ddad0a5a74efac3b079f1e6ceac3179e
chengcheng.wang <chengcheng.wang@garena.cn>
2021-11-24 16:32:35 +0800

    init5

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="hello-docker-jenkins4"/>
+    <HelloWorld msg="hello-docker-jenkins5"/>
   </div>
 </template>
 
